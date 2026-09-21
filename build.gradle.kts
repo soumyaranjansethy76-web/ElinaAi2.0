340350611b9c18a7ab7ad2b674fc3dd6aa828ab7
soumyaranjansethy76-web <soumyaranjansethy76@gmail.com>
2026-09-21 20:06:20 +0530

    Migrate root build to AGP 9 built-in Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("com.android.application") version "9.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("com.google.devtools.ksp") version "2.2.21-2.0.5" apply false
+    id("com.google.devtools.ksp") version "2.3.10" apply false
 }
